9987c74ce72fa034ecf27be47379912b5127295f
Ziyang Wen <wenziyang007@gmail.com>
2024-06-05 11:52:38 +0800

    Change meals but failed for changing status

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -9,9 +9,18 @@ import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface SetmealMapper {
+
+    /**
+     * 套餐的起售停售
+     * @param setmeal
+     */
+    @Autofill(OperationType.UPDATE)
+    @Update("update setmeal set status = #{status},update_user = #{updateUser},update_time = #{updateTime}")
+    void StartOrStop(Setmeal setmeal);
 
     /**
      * 根据分类id查询套餐的数量
@@ -50,5 +59,6 @@ public interface SetmealMapper {
     @Delete("delete from setmeal where id = #{id}")
     void deleteById(Long setmealId);
 
+    @Autofill(OperationType.UPDATE)
     void update(Setmeal setmeal);
 }
